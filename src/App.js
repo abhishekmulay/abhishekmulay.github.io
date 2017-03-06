fe66bcee3ebe6c924a1f43244c7fde5229e68dc7
Abhishek Mulay <mulay.a@husky.neu.edu>
2017-03-06 00:55:42 -0500

    New react blog

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './homepage/HomePage.js';
+
+class App extends Component {
+    render() {
+        return (
+            <HomePage></HomePage>
+        );
+    }
+}
+
+export default App;
